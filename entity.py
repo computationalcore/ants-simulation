ad17f55024523593f0473c2aec9794ab9279d9ca
Vinícius <computationalcore@gmail.com>
2015-12-08 20:56:45 -0200

    Add base simulation entity

---
+++ entity.py
@@ -0,0 +1,22 @@
+
+from gameobjects.vector2 import Vector2
+
+
+class SimulationEntity(object):
+    """
+    The Base Class for the simulation Entity.
+    """
+    def __init__(self, world, name, image):
+        self.world = world
+        self.name = name
+        self.image = image
+        self.location = Vector2(0, 0)
+        self.destination = Vector2(0, 0)
+        self.speed = 0.
+        self.id = 0
+
+    def render(self, surface):
+        x, y = self.location
+        w, h = self.image.get_size()
+        surface.blit(self.image, (x - w / 2, y - h / 2))
+
