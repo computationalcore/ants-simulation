1169a2c51475d988b1fc18083246f8a71e4bbfea
Vinícius <computationalcore@gmail.com>
2015-12-08 21:34:20 -0200

    Add brain to simulation entity and base state machine

--- entity.py
+++ entity.py
@@ -1,5 +1,5 @@
-
 from gameobjects.vector2 import Vector2
+from state import StateMachine
 
 
 class SimulationEntity(object):
@@ -14,9 +14,20 @@ class SimulationEntity(object):
         self.destination = Vector2(0, 0)
         self.speed = 0.
         self.id = 0
+        self.brain = StateMachine()
 
     def render(self, surface):
         x, y = self.location
         w, h = self.image.get_size()
         surface.blit(self.image, (x - w / 2, y - h / 2))
+
+    def process(self, time_passed):
+        self.brain.think()
+
+        if self.speed > 0. and self.location != self.destination:
+            vec_to_destination = self.destination - self.location
+            distance_to_destination = vec_to_destination.get_length()
+            heading = vec_to_destination.get_normalized()
+            travel_distance = min(distance_to_destination, time_passed * self.speed)
+            self.location += travel_distance * heading
 
---
+++ state.py
@@ -0,0 +1,28 @@
+class StateMachine(object):
+    def __init__(self):
+
+        self.states = {}
+        self.active_state = None
+
+    def add_state(self, state):
+
+        self.states[state.name] = state
+
+    def think(self):
+
+        if self.active_state is None:
+            return
+
+        self.active_state.do_actions()
+
+        new_state_name = self.active_state.check_conditions()
+        if new_state_name is not None:
+            self.set_state(new_state_name)
+
+    def set_state(self, new_state_name):
+
+        if self.active_state is not None:
+            self.active_state.exit_actions()
+
+        self.active_state = self.states[new_state_name]
+        self.active_state.entry_actions()
