1c363683e88ba00b22cb587c483a5e01f665c5ea
Vinícius <computationalcore@gmail.com>
2015-12-09 21:41:40 -0200

    Add ant entity class

--- entity.py
+++ entity.py
@@ -6,6 +6,7 @@ class SimulationEntity(object):
     """
     The Base Class for the simulation Entity.
     """
+
     def __init__(self, world, name, image):
         self.world = world
         self.name = name
@@ -31,3 +32,46 @@ class SimulationEntity(object):
             travel_distance = min(distance_to_destination, time_passed * self.speed)
             self.location += travel_distance * heading
 
+
+class Ant(SimulationEntity):
+    """
+    The Ant Entity Class
+    """
+
+    def __init__(self, world, image):
+
+        SimulationEntity.__init__(self, world, "ant", image)
+
+        '''
+        exploring_state = AntStateExploring(self)
+        seeking_state = AntStateSeeking(self)
+        delivering_state = AntStateDelivering(self)
+        hunting_state = AntStateHunting(self)
+
+        self.brain.add_state(exploring_state)
+        self.brain.add_state(seeking_state)
+        self.brain.add_state(delivering_state)
+        self.brain.add_state(hunting_state)'''
+
+        self.carry_image = None
+
+    def carry(self, image):
+
+        self.carry_image = image
+
+    def drop(self, surface):
+
+        if self.carry_image:
+            x, y = self.location
+            w, h = self.carry_image.get_size()
+            surface.blit(self.carry_image, (x - w, y - h / 2))
+            self.carry_image = None
+
+    def render(self, surface):
+
+        SimulationEntity.render(self, surface)
+
+        if self.carry_image:
+            x, y = self.location
+            w, h = self.carry_image.get_size()
+            surface.blit(self.carry_image, (x - w, y - h / 2))
