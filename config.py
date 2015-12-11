b9a08b5bb5a480709d19bc2a8b90861197954bf7
Vinícius <computationalcore@gmail.com>
2015-12-11 14:58:24 -0200

    Add app.cfg and update config

--- config.py
+++ config.py
@@ -6,5 +6,10 @@ import ConfigParser
 
 dir_name = os.path.abspath(os.path.dirname(__file__))
 
-config = ConfigParser.ConfigParser()
-config.read(os.path.join(dir_name, 'app.cfg'))
+config = ConfigParser.RawConfigParser()
+config.read( 'app.cfg')
+
+ANT_COUNT = config.getint('general', 'ants')
+SCREEN_SIZE = (config.getint('general', 'screen_size_x'), config.getint('general', 'screen_size_y'))
+NEST_POSITION = (config.getint('nest', 'position_x'), config.getint('nest', 'position_y'))
+NEST_SIZE = config.getfloat('nest', 'size')
